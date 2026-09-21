0383b234e34b78e0a223a6ee09c49fc0d8eb242f
hi-anki <managemee@protonmail.com>
2026-09-21 09:42:21 +0530

    Changed post description limit to 200 characters.

--- src/content.config.ts
+++ src/content.config.ts
@@ -18,7 +18,7 @@ const post = defineCollection({
 					src: image(),
 				})
 				.optional(),
-			description: z.string().min(10).max(160),
+			description: z.string().min(10).max(200),
 			draft: z
 				.boolean()
 				.or(
